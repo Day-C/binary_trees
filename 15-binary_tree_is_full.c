23d19218e03c24ffb94adbc0d4ba3bf77cb53314
DayC <kongranalanyuy@gmail.com>
2024-04-01 09:02:02 -0700

    check if parent nod is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,11 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		 return (1);
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 	left_full = binary_tree_is_full(tree->left);
 	right_full = binary_tree_is_full(tree->right);
-	
+
 	return (1 * left_full * right_full);
 }
