0edfad56cdfe2e195376b131f61f5784cd9c9296
DayC <kongranalanyuy@gmail.com>
2024-04-01 07:33:30 -0700

    height of node on binary_tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
 	else
 	{
 		left_hight = binary_tree_height(tree->left);
