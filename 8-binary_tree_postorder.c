ed138eb65f3ff479543f6298bcdc3d97250225c4
DayC <kongranalanyuy@gmail.com>
2024-04-01 06:13:43 -0700

    travers binary tree with postorder technique

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - function disolays a binary tree with the postordert traversal texhnique
+ * binary_tree_postorder - travers binary tree with postorder technique
  * @tree: pointer to root node
  * @func: function pointer to functiin that displays items
  * Return: return nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
 	binary_tree_postorder(tree->left, func);
